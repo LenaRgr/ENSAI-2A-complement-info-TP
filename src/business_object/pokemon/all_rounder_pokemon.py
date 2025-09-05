9d20f5c6525da4091bd6db18d6ab7d15a89c8ee0
lenargr <lena.roger@eleve.ensai.fr>
2025-09-05 09:19:53 +0000

    sauvegarde

---
+++ src/business_object/pokemon/all_rounder_pokemon.py
@@ -0,0 +1,5 @@
+from business_object.statistic import Statistic
+from abc import ABC, abstractmethod
+from business_object.pokemon.abstract_pokemon import AbstractPokemon
+
+class AllRounderPokemon(AbstractPokemon):
